f707f77e44ebbbac6196949916660424a0482103
Damir-Alexis-Chapal <dchapalimbaquin2018@gmail.com>
2025-09-01 13:41:08 -0500

    The Spring boot project has been created

---
+++ lodging_house/src/main/java/com/app_lodging_house/lodging_house/LodgingHouseApplication.java
@@ -0,0 +1,13 @@
+package com.app_lodging_house.lodging_house;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LodgingHouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LodgingHouseApplication.class, args);
+    }
+
+}
---
+++ lodging_house/src/test/java/com/app_lodging_house/lodging_house/LodgingHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app_lodging_house.lodging_house;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LodgingHouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
